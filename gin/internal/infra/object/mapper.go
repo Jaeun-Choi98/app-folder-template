21172fdb3918e99b3e09bad04d8fe602ff95024a
Jaeun-Choi98 <wodns617@gmail.com>
2025-06-24 17:55:15 +0900

    .

---
+++ gin/internal/infra/object/mapper.go
@@ -0,0 +1,6 @@
+package object
+
+const (
+	NULL_NUMBER = -127
+	NULL_STRING = "NULL_STR"
+)
--- gin/internal/transport/http-rest/request/sample_request.go
+++ gin/internal/transport/http-rest/request/sample_request.go
@@ -1,1 +1,9 @@
 package request
+
+/**
+ * To check whether it is an empty value or not after unmarshal, a pointer type is used.
+ * If it is not a pointer type, the default value of the corresponding data type is entered.
+ * e.g. number -> 0, string -> "", array -> ["",""], slice -> []
+ *
+ * cf) It is essential to check for nil before referencing the converted data later.
+ */
