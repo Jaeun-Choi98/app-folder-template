b9c8b9f9dc4166e65838f1479ada283dcf0948b9
Jaeun-Choi98 <wodns617@gmail.com>
2025-10-02 13:20:06 +0900

    .

--- gin/docker_build.sh
+++ gin/docker_build.sh
@@ -4,3 +4,4 @@ sudo docker build -t smart_shelter:latest ./
 # 사설 레지스트리
 sudo docker tag smart_shelter:latest 10.1.0.118:443/smart_shelter:latest
 sudo docker push 10.1.0.118:443/smart_shelter:latest
+sudo docker system prune -f 
